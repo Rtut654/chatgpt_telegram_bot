6fc04b122328691d4cf15c2f6e5ffff473a52484
Andrey Samoylenko <andreysamsaa@gmail.com>
2024-12-27 06:37:49 +0300

    feat: separate messages from logic

---
+++ src_bot/bot/messages.py
@@ -0,0 +1,12 @@
+payment_message = """
+Please select a plan from options below. 
+Subscription plans renew at the end of each period. 
+
+You can cancel subscription anytime by sending /balance command and hitting Unsubscribe button. Contact support via @testsucceed
+"""
+
+subscribe_desc_by_tariff = {
+    "1": "<b>✅ Subscription gives you access to 100 daily messages for 1 month.</b>",
+    "6": "<b>✅ Subscription gives you access to 100 daily messages for 6 months.</b>",
+    "12": "<b>✅ Subscription gives you access to 100 daily messages for 1 year.</b>"
+}
